33aac53bd00339b8bcf82a7cb2305e17fc6550ee
leolaz99 <leolaz99@gmail.com>
2020-12-28 17:53:15 +0100

    Add movement and shot

---
+++ Assets/Scripts/Joystick.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class Joystick : MonoBehaviour
+{
+    public Transform player;
+    public float speed = 5.0f;
+    bool touchStart = false;
+    public GameObject prefab;
+    Vector3 pointA;
+    Vector3 pointB;
+
+    void moveCharacter(Vector3 direction)
+    {
+        player.Translate(direction * speed * Time.deltaTime);
+    }
+
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                if (Input.touches[i].position.x < Screen.width / 2)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        pointA = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+                    }
+
+                    if (Input.GetMouseButton(0))
+                    {
+                        touchStart = true;
+                        pointB = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+                    }
+
+                    else
+                    {
+                        touchStart = false;
+                    }
+                }
+
+                if (Input.touches[i].position.x > Screen.width / 2)
+                {
+                    Instantiate(prefab, player.position, player.rotation);
+                }
+            }          
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector3 offset = pointB - pointA;
+            Vector3 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction * -1);
+        }
+    } 
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Translate(Vector3.right * 5 * Time.deltaTime);
+    }
+}
